3f8fa58a05fa3fc73f95c4f73a2aff3d8af431cc
chige12 <tige1215.technic@gmail.com>
2021-03-17 16:07:09 +0900

    first commit

---
+++ src/renderer/nuxt.config.js
@@ -0,0 +1,81 @@
+const isProduction = process.env.NODE_ENV === 'production'
+const isDev = process.env.NODE_ENV === 'development'
+
+export default {
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: false,
+
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'kc3hack-front',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    'vuesax/dist/vuesax.css'
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+    '@/plugins/vuesax'
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/typescript
+    '@nuxt/typescript-build',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+  ],
+
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {},
+
+  dev: isDev,
+  router: {
+    mode: 'hash',
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+    vendor: [
+      'socket.io-client'
+    ],
+    extend(config) {
+      if (!isDev) {
+        // absolute path to files on production (default value: '/_nuxt/')
+        config.output.publicPath = '_nuxt/'
+      }
+      config.node = {
+        __dirname: !isProduction,
+        __filename: !isProduction,
+      }
+    },
+  },
+  generate: {
+    dir: '../../dist/nuxt-build',
+  },
+  telemetry: false,
+}
